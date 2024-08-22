8ffeb9f4fdbaa3df32c02ae37870edc46241fb4c
Fbotdc <f.botdccc@gmail.com>
2024-08-22 18:53:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 
 
 
-const statusMessages = ["Features coming soon!"]; /** do what you want here*/
+const statusMessages = ["Features coming soon!","Soon im the best Pulling Bot!"]; /** do what you want here*/
 
 
 let currentIndex = 0;
